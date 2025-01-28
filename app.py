dec4ae43a5bc1486e48ac54cd78ec74c93508aed
Matheus Romagnoli <matheuszothavio@gmail>
2025-01-28 11:17:16 -0300

    ADD: adição das páginas sobre e INICIAL, ajuste de textos, listas no python e styles

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
-# AQUI IRÁ TODAS AS MINHAS ROTAS
+# Lista de cores para alterar o fundo do site
+lista_cores = ["#FACADA",
+               "#BABACA",
+               "#9CFFAF",
+               "E2ED98"]
 
-app.run()
+# Lista de frases para a pagina inicial
+lista_frases = ["Transforme suas ideias em sons. DAWs: a chave para a criatividade musical.",
+                "Cada batida, cada melodia. Domine a arte da produção musical com DAWs.",
+                "A música começa aqui. Descubra o poder das DAWs.",
+                "Inspire-se, componha e edite. As DAWs tornam a produção musical acessível."]
+
+# AQUI IRÁ TODAS AS MINHAS ROTAS
+@app.route("/")
+def pagina_inicial():
+    cor_de_fundo = random.choice(lista_cores)
+    return  render_template("inicial.html", cor_de_fundo_html = cor_de_fundo)
+
+
+@app.route("/sobre")
+def pagina_sobre():
+    cor_de_fundo = random.choice(lista_cores)
+    return  render_template("sobre.html", cor_de_fundo_html = cor_de_fundo)
+    
+
+app.run(debug=True, host="0.0.0.0", port=8080)
