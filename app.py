d5741fbeabe46d025187b51473853625bdabdc25
Matheus Romagnoli <matheuszothavio@gmail.com>
2025-02-18 11:07:54 -0300

    ADD: adição de um arquivo novo para as listas

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
 from flask import Flask, render_template, request, redirect, request
 import random
+from .data import lista_configuracao as listas
 
 app = Flask(__name__)
-
-# Lista de cores para alterar o fundo do site
-lista_cores = ["#FACADA",
-               "#BABACA",
-               "#9CFFAF",
-               "E2ED98"]
-
-# Lista de frases para a pagina inicial
-lista_frases = ["Transforme suas ideias em sons. DAWs: a chave para a criatividade musical.",
-                "Cada batida, cada melodia. Domine a arte da produção musical com DAWs.",
-                "A música começa aqui. Descubra o poder das DAWs.",
-                "Inspire-se, componha e edite. As DAWs tornam a produção musical acessível."]
 
 # AQUI IRÁ TODAS AS MINHAS ROTAS
 @app.route("/", methods=["GET"])
 def pagina_inicial():
-    cor_de_fundo = random.choice(lista_cores)
-    frase = random.choice(lista_frases)
+    cor_de_fundo = random.choice(listas.lista_cores)
+    frase = random.choice(listas.lista_frases)
     return  render_template("inicial.html",
                             cor_de_fundo_html = cor_de_fundo,
                             frase_html = frase )
@@ -27,31 +16,31 @@ def pagina_inicial():
 
 @app.route("/sobre")
 def pagina_sobre():
-    cor_de_fundo = random.choice(lista_cores)
+    cor_de_fundo = random.choice(listas.lista_cores)
     return  render_template("sobre.html",
                             cor_de_fundo_html = cor_de_fundo)
 
 @app.route("/cadastro", methods=["GET"])
 def pagina_cadastro():
-    cor_de_fundo = random.choice(lista_cores)
+    cor_de_fundo = random.choice(listas.lista_cores)
     return  render_template("cadastro.html",
-                            lista_frases_html = lista_frases)
+                            lista_frases_html = listas.lista_frases)
     
 
 @app.route("/post/cadastrarfrase", methods=["POST"])
 def post_cadastrarfrase():
     frase_vinda_do_html = request.form.get("frase")
-    lista_frases.append(frase_vinda_do_html)
+    listas.lista_frases.append(frase_vinda_do_html)
     return redirect("/cadastro")
 
 @app.route("/cores", methods=["GET"])
 def pagina_cores():
-    return render_template("cores.html", lista_cores_html=lista_cores)
+    return render_template("cores.html", lista_cores_html=listas.lista_cores)
 
 @app.route("/cadastrarcores", methods=["POST"])
 def pagina_cadastrar_cores():
     cor = request.form.get("cor")
-    lista_cores.append(cor)
+    listas.lista_cores.append(cor)
     return redirect("/cores")
 
 @app.route("/cores/delete/<indice_cor>")
@@ -59,7 +48,7 @@ def delete_cores(indice_cor):
     # converte o índice pra numero inteiro (ele vem como string)
     indice_cor = int(indice_cor)
     # exclui a cor da lista pelo índice
-    lista_cores.pop(indice_cor)
+    listas.lista_cores.pop(indice_cor)
     # redireciona para a pagina/rota /cores
     return redirect("/cores")
 
---
+++ data/lista_configuracao.py
@@ -0,0 +1,11 @@
+# Lista de cores para alterar o fundo do site
+lista_cores = ["#FACADA",
+               "#BABACA",
+               "#9CFFAF",
+               "E2ED98"]
+
+# Lista de frases para a pagina inicial
+lista_frases = ["Transforme suas ideias em sons. DAWs: a chave para a criatividade musical.",
+                "Cada batida, cada melodia. Domine a arte da produção musical com DAWs.",
+                "A música começa aqui. Descubra o poder das DAWs.",
+                "Inspire-se, componha e edite. As DAWs tornam a produção musical acessível."]
